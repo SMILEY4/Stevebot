e66f1702bcfa2ce841c9743f2dee9034823a8aeb
Lukas Ruegner <ruegnerlukas@gmail.com>
2019-10-02 22:36:46 +0200

    Added test for goals

---
+++ src/test/java/stevebot/GoalTests.java
@@ -0,0 +1,79 @@
+package stevebot;
+
+import org.junit.jupiter.api.Test;
+import stevebot.data.blockpos.BaseBlockPos;
+import stevebot.pathfinding.goal.ExactGoal;
+import stevebot.pathfinding.goal.XZGoal;
+
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GoalTests {
+
+
+	private static final Random random = new Random();
+
+
+
+
+	@Test
+	void testXZGoal() {
+		final int x = random.nextInt();
+		final int z = random.nextInt();
+		XZGoal goal = new XZGoal(x, z);
+		assertThat(goal.x).isEqualTo(x);
+		assertThat(goal.z).isEqualTo(z);
+	}
+
+
+
+
+	@Test
+	void testReachedXZGoal() {
+		final int x0 = random.nextInt();
+		final int y0 = random.nextInt();
+		final int z0 = random.nextInt();
+		final int x1 = random.nextInt();
+		final int y1 = random.nextInt();
+		final int z1 = random.nextInt();
+		XZGoal goal = new XZGoal(x0, z0);
+		BaseBlockPos blockPos0 = new BaseBlockPos(x0, y0, z0);
+		BaseBlockPos blockPos1 = new BaseBlockPos(x1, y1, z1);
+		assertThat(goal.reached(blockPos0)).isTrue();
+		assertThat(goal.reached(blockPos1)).isFalse();
+	}
+
+
+
+
+	@Test
+	void testExactGoal() {
+		final int x = random.nextInt();
+		final int y = random.nextInt();
+		final int z = random.nextInt();
+		ExactGoal goal = new ExactGoal(new BaseBlockPos(x, y, z));
+		assertThat(goal.pos.getX()).isEqualTo(x);
+		assertThat(goal.pos.getY()).isEqualTo(y);
+		assertThat(goal.pos.getZ()).isEqualTo(z);
+	}
+
+
+
+
+	@Test
+	void testReachedExactGoal() {
+		final int x0 = random.nextInt();
+		final int y0 = random.nextInt();
+		final int z0 = random.nextInt();
+		final int x1 = random.nextInt();
+		final int y1 = random.nextInt();
+		final int z1 = random.nextInt();
+		ExactGoal goal = new ExactGoal(new BaseBlockPos(x0, y0, z0));
+		BaseBlockPos blockPos0 = new BaseBlockPos(x0, y0, z0);
+		BaseBlockPos blockPos1 = new BaseBlockPos(x1, y1, z1);
+		assertThat(goal.reached(blockPos0)).isTrue();
+		assertThat(goal.reached(blockPos1)).isFalse();
+	}
+
+}
