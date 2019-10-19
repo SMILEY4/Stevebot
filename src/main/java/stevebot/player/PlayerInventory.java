80cd7f51790c8896dc61745575807895b4c425f2
Lukas Ruegner <ruegnerlukas@gmail.com>
2019-10-19 21:19:08 +0200

    Document PlayerInventory

--- src/main/java/stevebot/player/PlayerInventory.java
+++ src/main/java/stevebot/player/PlayerInventory.java
@@ -6,10 +6,19 @@ import stevebot.data.items.wrapper.ItemWrapper;
 public interface PlayerInventory {
 
 
+	/**
+	 * @return a new {@link InventorySnapshot} with the current content of the player
+	 */
 	InventorySnapshot createSnapshotFromPlayerEntity();
 
+	/**
+	 * @return the {@link InventorySnapshot} set with {@link PlayerInventory#setCurrentSnapshot(InventorySnapshot)}
+	 */
 	InventorySnapshot getCurrentSnapshot();
 
+	/**
+	 * @param snapshot sets the snapshot. This snapshot can be retrieved with {@link PlayerInventory#getCurrentSnapshot()}
+	 */
 	void setCurrentSnapshot(InventorySnapshot snapshot);
 
 	/**
@@ -19,6 +28,12 @@ public interface PlayerInventory {
 	 */
 	boolean selectThrowawayBlock();
 
+	/**
+	 * Selects the slot containing the given item
+	 *
+	 * @param item the item to select
+	 * @return true, if the item was selected
+	 */
 	boolean selectItem(ItemWrapper item);
 
 
