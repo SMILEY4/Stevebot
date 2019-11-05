66f571dc82fda2d55b2c3dd65cd793b6cb385678
Lukas Ruegner <ruegnerlukas@gmail.com>
2019-11-05 21:25:16 +0100

    Set version for dev-builds

--- src/main/java/stevebot/misc/Config.java
+++ src/main/java/stevebot/misc/Config.java
@@ -7,8 +7,8 @@ public class Config {
 
 
 	public static final String MODID = "stevebot";
-	public static final String NAME = "SteveBot";
-	public static final String VERSION = "0.1-dev";
+	public static final String NAME = "Stevebot";
+	public static final String VERSION = "dev";
 	public static final String MC_VERSION = "[1.12.2]";
 
 	private static float pathfindingTimeoutSeconds = 10;
