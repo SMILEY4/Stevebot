b411bb539e260d561acc887c48a83069afe271b3
Lukas Ruegner <ruegnerlukas@gmail.com>
2019-11-24 13:49:09 +0100

    Fix function: place block against

--- src/main/java/stevebot/pathfinding/actions/ActionUtils.java
+++ src/main/java/stevebot/pathfinding/actions/ActionUtils.java
@@ -230,7 +230,7 @@ public class ActionUtils {
 	 */
 	public static boolean placeBlockAgainst(BaseBlockPos pos, Direction direction) {
 		if (BlockUtils.canWalkOn(pos) && facesPlayer(pos, direction)) {
-			PlayerUtils.getCamera().setLookAt(pos);
+			PlayerUtils.getCamera().setLookAtBlockSide(pos, direction);
 			PlayerUtils.getInput().setPlaceBlock();
 			return true;
 		} else {
